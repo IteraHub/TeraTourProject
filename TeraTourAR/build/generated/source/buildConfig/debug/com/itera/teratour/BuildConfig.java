f747dba35f2923d064a9e9c56ffefaabaad96e29
Francis Anyeji <anyeji.francis@gmail.com>
2019-10-14 13:35:09 +0100

    Updated gradle configuration

--- TeraTourAR/build/generated/source/buildConfig/debug/com/itera/teratour/BuildConfig.java
+++ TeraTourAR/build/generated/source/buildConfig/debug/com/itera/teratour/BuildConfig.java
@@ -5,6 +5,11 @@ package com.itera.teratour;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.itera.teratour";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
   public static final String APPLICATION_ID = "com.itera.teratour";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
